faaedbaffbca873795027a10bcb831c576a0fa17
Ridhoini ABOUBACARI <aboubacari.ridhoini@gmail.com>
2016-11-19 22:44:56 +0100

    refactor: add link to use material design

--- ExpanseReportManager/App_Start/BundleConfig.cs
+++ ExpanseReportManager/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace ExpanseReportManager
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/Content/bootstrap_material/js").Include(
+                        "~/Content/bootstrap_material/js/material.min.js",
+                        "~/Content/bootstrap_material/js/ripples.min.js"));
+
             // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
             // prêt pour la production, utilisez l'outil de génération (bluid) sur http://modernizr.com pour choisir uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +30,13 @@ namespace ExpanseReportManager
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap_material/css").Include(
+                        "~/Content/bootstrap_material/css/bootstrap-material-design.min.css",
+                        "~/Content/bootstrap_material/css/ripples.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/own").Include(
+                        "~/Content/style.css"));
         }
     }
 }

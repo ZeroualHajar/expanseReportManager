23db96bc3f52c6f39b1fadbc4b00c1ce35eb96d2
Ridhoini ABOUBACARI <aboubacari.ridhoini@gmail.com>
2016-11-21 21:46:25 +0100

    refactor: add getAll function

--- ExpanceReportManagerRepository/TvaRepository.cs
+++ ExpanceReportManagerRepository/TvaRepository.cs
@@ -10,6 +10,15 @@ namespace ExpanceReportManagerRepository
     class TvaRepository
     {
         public NotesDeFraisEntities entities;
+        public TvaRepository(NotesDeFraisEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public IQueryable<Tva> getAll()
+        {
+            return entities.Tvas;
+        }
 
     }
 }

3b6d6ebc9f803c8bac1e190ce3b4e1c133872ed1
Hajar ZEROUAL <hajar.zeroual@hotmail.com>
2016-11-26 22:55:34 +0100

    feat: create poleService

---
+++ ExpanseReportManager/Services/PoleService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ExpanseReportManagerModel;
+using ExpanseReportManager.Mapping;
+using ExpanseReportManager.Models;
+using ExpanseReportManager.Repositories;
+
+namespace ExpanseReportManager.Services
+{
+    public class PoleService
+    {
+        public PoleMapper Mapper;
+        public PoleRepository Repository;
+
+        public PoleService()
+        {
+            this.Mapper = new PoleMapper();
+            this.Repository = new PoleRepository(new NotesDeFraisEntities());
+        }
+
+        public List<PoleViewModels> GetAll()
+        {
+            List<PoleViewModels> result = new List<PoleViewModels>();
+
+            IQueryable<Pole> poles = Repository.GetAll();
+            foreach(Pole res in poles)
+            {  
+                result.Add(Mapper.DataToModel(res)) ;
+            }
+
+            return result;
+        }
+    }
+}
+

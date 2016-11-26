bfecb9e41c7809b315d6455a5a44a9bde109cab1
Hajar ZEROUAL <hajar.zeroual@hotmail.com>
2016-11-26 22:45:15 +0100

    feat: create pole repository

---
+++ ExpanseReportManager/Repositories/PoleRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ExpanseReportManagerModel;
+
+namespace ExpanseReportManager.Repositories
+{
+    public class PoleRepository
+    {
+        public NotesDeFraisEntities entities;
+        public PoleRepository(NotesDeFraisEntities entities)
+        {
+            this.entities = entities;
+        }
+
+        public IQueryable<Pole> GetAll()
+        {
+            return entities.Poles;
+        }
+
+        public Pole GetById(Guid id)
+        {
+            return GetAll().FirstOrDefault(p => p.Pole_ID == id);
+        }
+
+        public void Add(Pole pole)
+        {
+            entities.Poles.Add(pole);
+        }
+
+        public void Delete(Pole pole)
+        {
+            entities.Poles.Remove(pole);
+        }
+
+        public void Save()
+        {
+            entities.SaveChanges();
+        }
+    }
+}

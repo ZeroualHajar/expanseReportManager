be6778cc0abbed24743e42a397a70f0976ec7274
Hajar ZEROUAL <hajar.zeroual@hotmail.com>
2016-11-26 22:53:03 +0100

    feat : creat PoleViewModel

---
+++ ExpanseReportManager/Models/PoleViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpanseReportManager.Models
+{
+    public class PoleViewModels
+    {
+        [Display(Name = "Id")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Nom du pôle")]
+        public string Name { get; set; }
+
+        [Display(Name = "Manager id")]
+        public Guid ManagerId { get; set; }
+
+
+    }
+}
+

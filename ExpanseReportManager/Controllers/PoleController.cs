ac689a8cc176b8fcc3d301f6893c6f1e85d1e27c
Hajar ZEROUAL <hajar.zeroual@hotmail.com>
2016-11-26 22:57:13 +0100

    feat: creat poleController

---
+++ ExpanseReportManager/Controllers/PoleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExpanseReportManager.Services;
+using ExpanseReportManager.Models;
+using ExpanseReportManager.Repositories;
+using ExpanseReportManager.Mapping;
+
+namespace ExpanseReportManager.Controllers
+{
+    public class PoleController : Controller
+    {
+        public PoleService PoleService;
+
+        public PoleController()
+        {
+            this.PoleService = new PoleService();
+        } 
+        // GET: Pole
+        public ActionResult Index()
+        {
+            List<PoleViewModels> list = PoleService.GetAll();
+            return View(list);
+        }
+    }
+}
+

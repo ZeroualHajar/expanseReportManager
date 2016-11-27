bc4afd92f73464f580b189c9cb4d95262d575f8e
Ridhoini ABOUBACARI <aboubacari.ridhoini@gmail.com>
2016-11-27 10:04:38 +0100

    refactor: add attribute to roleModel

--- ExpanseReportManager/Models/RoleViewModels.cs
+++ ExpanseReportManager/Models/RoleViewModels.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExpanseReportManager.Models
 {
     public class RoleViewModels
     {
+        [Display(Name = "Id Role")]
+        public string Id { get; set; }
+
+        [Display(Name = "Nom Role")]
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        public ICollection<EmployeeViewModels> AspNetUsers { get; set; }
     }
 }

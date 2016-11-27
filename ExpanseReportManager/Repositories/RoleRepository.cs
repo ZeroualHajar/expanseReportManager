18785869b2d5d329868b3040447de7469ad42129
Ridhoini ABOUBACARI <aboubacari.ridhoini@gmail.com>
2016-11-27 10:03:47 +0100

    feat: create role repository

---
+++ ExpanseReportManager/Repositories/RoleRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ExpanseReportManagerModel;
+
+namespace ExpanseReportManager.Repositories
+{
+    public class RoleRepository
+    {
+        public NotesDeFraisEntities Entities;
+        public RoleRepository(NotesDeFraisEntities entities)
+        {
+            this.Entities = entities;
+        }
+
+        public AspNetRole GetById(String id)
+        {
+            return Entities.AspNetRoles.FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Add(AspNetRole role)
+        {
+            Entities.AspNetRoles.Add(role);
+        }
+
+        public void Delete(AspNetRole role)
+        {
+            Entities.AspNetRoles.Remove(role);
+        }
+
+        public void Save()
+        {
+            Entities.SaveChanges();
+        }
+    }
+}
